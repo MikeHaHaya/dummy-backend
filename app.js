58eea6462231f0cb6a0b89dce142010dff527bb3
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-02-15 17:58:29 +0200

    More dummy stuff

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use((req, res, next) => {
 
 app.get('/posts', async (req, res) => {
   const storedPosts = await getStoredPosts();
-  // await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
+  // Meant to imitate loading time from a server
+  await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
   res.json({ posts: storedPosts });
 });
 
